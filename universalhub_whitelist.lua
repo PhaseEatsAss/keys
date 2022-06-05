232aecf6f3dcac3b73d4f9fb6f4a3da92a6757a8
PhaseEatsAss <102885769+PhaseEatsAss@users.noreply.github.com>
2022-06-05 15:09:26 +0100

    Create universalhub_whitelist.lua

---
+++ universalhub_whitelist.lua
@@ -0,0 +1,14 @@
+local whitelisted = false
+local whitelist = {
+"2620334011", //n0taf4k3
+"260729679", //super1687
+"2845981809", //midokfm2020alt3
+"2845980527", //midokfm2020alt2
+"622786740" //Wrap234
+}
+
+for i, v in ipairs(whitelist) do
+    if v == game.Players.LocalPlayer.UserId then
+        whitelisted = true
+    end
+end
